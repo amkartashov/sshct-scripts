4a1cf4d9a2293c94ce1b458b83e2d1e5009e11ac
Andrey Kartashov <gorilych@gmail.com>
2020-04-13 12:37:33 +0700

    add bazelisk 1.3.0

---
+++ install.bazelisk.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+version=v1.3.0
+dist=https://github.com/bazelbuild/bazelisk/releases/download/${version}/bazelisk-linux-amd64
+file=/usr/local/bin/bazelisk
+
+wget -O ${file} -c ${dist}
+
+chmod +x ${file}
+
