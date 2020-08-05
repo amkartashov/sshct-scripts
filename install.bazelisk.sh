1104c3774d7c73b31c2406669b985da4d1992717
Andrey Kartashov <gorilych@gmail.com>
2020-08-05 13:29:00 +0700

    use bazelisk as bazel

--- install.bazelisk.sh
+++ install.bazelisk.sh
@@ -2,7 +2,7 @@
 
 version=v1.5.0
 dist=https://github.com/bazelbuild/bazelisk/releases/download/${version}/bazelisk-linux-amd64
-file=/usr/local/bin/bazelisk
+file=/usr/local/bin/bazel
 
 wget -O ${file} -c ${dist}
 
