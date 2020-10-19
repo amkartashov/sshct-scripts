2f3496a772777fdc488127f199e1bc3ef35fbfcb
Andrey Kartashov <gorilych@gmail.com>
2020-10-19 16:02:43 +0700

    add bazel buildtools

---
+++ install.bazel-buildtools.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+version=3.5.0
+dist=https://github.com/bazelbuild/buildtools/releases/download/${version}
+
+for exe in buildifier buildozer unused_deps; do
+  wget -O /usr/local/bin/${exe} -c ${dist}/${exe}
+  chmod +x /usr/local/bin/${exe}
+done
+
