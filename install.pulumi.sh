3fdfafaa6405f3cec6d831037370f10a3d3ecf73
Andrey Kartashov <gorilych@gmail.com>
2020-12-08 18:07:30 +0700

    pulumi: upgrade to 2.15.3

--- install.pulumi.sh
+++ install.pulumi.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-version=2.13.2
+version=2.15.3
 dist=https://get.pulumi.com/releases/sdk/pulumi-v${version}-linux-x64.tar.gz
-archive=${dist##*/}
 
-wget -O /tmp/${archive} -c ${dist}
-
-tar -C /usr/local/bin --strip-components=1 -xpf /tmp/${archive}
+wget -qO- -c ${dist} \
+| tar -C /usr/local/bin --strip-components=1 -xpzvf -
 
