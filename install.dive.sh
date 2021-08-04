3c37d256fbfa0ab2ec98daed1fe6fe2d8442a456
Andrey Kartashov <gorilych@gmail.com>
2021-08-04 12:00:48 +0700

    add dive

---
+++ install.dive.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+version=0.10.0
+dist=https://github.com/wagoodman/dive/releases/download/v${version}/dive_${version}_linux_amd64.deb
+
+wget -O /tmp/dive.deb -c ${dist}
+
+dpkg -i /tmp/dive.deb
+
+rm /tmp/dive.deb
