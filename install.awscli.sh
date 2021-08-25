3f4c848121570c145e0641a207a097f79e5545dd
Andrey Kartashov <gorilych@gmail.com>
2021-08-25 16:31:07 +0700

    upgrade aws-cli and pin version (2.2.32)

--- install.awscli.sh
+++ install.awscli.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-dist=https://s3.amazonaws.com/aws-cli/awscli-bundle.zip
+## https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html
 
-wget -O /tmp/awscli-bundle.zip -c "${dist}"
+version=2.2.32
+dist=https://awscli.amazonaws.com/awscli-exe-linux-x86_64-${version}.zip
 
-unzip -d /tmp /tmp/awscli-bundle.zip
+wget -O /tmp/awscliv2.zip -c "${dist}"
 
-/tmp/awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+unzip -d /tmp /tmp/awscliv2.zip
 
+/tmp/aws/install --install-dir /usr/local/aws-cli --bin-dir /usr/local/bin
 
+rm -rf /tmp/aws /tmp/awscliv2.zip
