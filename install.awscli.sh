ae1d3ea9e166bc50f939a9cbeda1d8d552e98a98
Andrey Kartashov <gorilych@gmail.com>
2022-03-18 19:43:57 +0700

    awscli use v2 latest

--- install.awscli.sh
+++ install.awscli.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
+source "${BASH_SOURCE%/*}/common.sh"
+
 ## https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html
 
-version=2.2.32
-dist=https://awscli.amazonaws.com/awscli-exe-linux-x86_64-${version}.zip
+dist=https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip
 
 wget -O /tmp/awscliv2.zip -c "${dist}" --quiet
 
 unzip -d /tmp /tmp/awscliv2.zip
 
-/tmp/aws/install --install-dir /usr/local/aws-cli --bin-dir /usr/local/bin
+/tmp/aws/install --install-dir /usr/local/aws-cli --bin-dir /usr/local/bin --update
 
 rm -rf /tmp/aws /tmp/awscliv2.zip
 
 ## https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html#install-plugin-debian
 
-curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "/tmp/session-manager-plugin.deb"
+install_from_url_deb \
+    https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb
 
-dpkg -i /tmp/session-manager-plugin.deb
-
-rm -rf /tmp/session-manager-plugin.deb
