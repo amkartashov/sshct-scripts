2f9c76042189293fa0811af2d2c6c576061a2a7e
Andrey Kartashov <gorilych@gmail.com>
2022-03-18 20:34:25 +0700

    linkerd-buoyant v0.8.0

--- install.linkerd.sh
+++ install.linkerd.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
+source "${BASH_SOURCE%/*}/common.sh"
+
 version=2.11.1
-dist=https://github.com/linkerd/linkerd2/releases/download/stable-${version}/linkerd2-cli-stable-${version}-linux-amd64
-file=/usr/local/bin/linkerd
 
-wget -O ${file} -c ${dist} --quiet
-
-chmod +x ${file}
+install_from_url \
+    https://github.com/linkerd/linkerd2/releases/download/stable-${version}/linkerd2-cli-stable-${version}-linux-amd64 \
+    linkerd
 
 # install buoyant extension
-version=v0.5.1
-dist=https://github.com/buoyantio/linkerd-buoyant/releases/download/${version}/linkerd-buoyant-${version}-linux-amd64
-file=/usr/local/bin/linkerd-buoyant
-
-wget -O ${file} -c ${dist} --quiet
-
-chmod +x ${file}
+version=v0.8.0
+install_from_url \
+    https://github.com/buoyantio/linkerd-buoyant/releases/download/${version}/linkerd-buoyant-${version}-linux-amd64 \
+    linkerd-buoyant
 
